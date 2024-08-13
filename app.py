12ab483b327769564beed8fb699562d304676515
ahmedeidabdulfattah <102394292+ahmedeidabdulfattah@users.noreply.github.com>
2024-08-14 01:30:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ import os
 if __name__ == '__main__':
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
